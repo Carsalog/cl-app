65b58255414b44d23f705d983bc7687fce65a458
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-23 15:43:51 -0700

    feat: Add auto generation of the states

--- server/startup/db.js
+++ server/startup/db.js
@@ -1,9 +1,30 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
 const config = require("config");
+const {State} = require("../models/states");
+const states = require("../data/states");
+const db = config.get("db");
 
-module.exports = function () {
-  const db = config.get("db");
-  mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => winston.info(`Connect to ${db}...`));
-};
+
+module.exports = () => mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(checkStates);
+
+
+function checkStates() {
+  State.getAll().then(result => {
+      if (!result.length) {
+        uploadStates();
+      } else {
+        winston.info(`Connect to ${db}...`)
+      }
+    }
+  );
+}
+
+function uploadStates() {
+  State.collection.insertMany(states, function (err) {
+    if (err) winston.error(`Cannot create states: ${err}`);
+  })
+}
