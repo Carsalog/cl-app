ca7b99972098c4760f587d603cfa802d5b481605
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-24 16:28:50 -0700

    feat: Add tests for ZipCard component

---
+++ client/src/components/base/ZipCard.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {ZipCard} from './ZipCard';
+
+describe('ZipCard', () => {
+
+  const props = {
+    zip: {
+      id: "00000",
+      city: {
+        name: 'test city'
+      },
+      state: {
+        abbreviation: "AA"
+      }
+    },
+    zipMsg: 'test message',
+    renderLink: jest.fn(),
+    zipConfirm: jest.fn()
+  };
+  const zipCard = shallow(<ZipCard {...props}/>);
+
+  it('should render properly', function () {
+    expect(zipCard).toMatchSnapshot();
+  });
+
+  it('should call renderLink', function () {
+    expect(props.renderLink)
+      .toHaveBeenCalledWith("confirm",
+        "/posts", props.zipConfirm)
+  });
+});
