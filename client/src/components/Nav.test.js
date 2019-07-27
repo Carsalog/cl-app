32f864e7e82f3a6f7772f9c96c3c4a14d51670b0
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-27 15:43:07 -0700

    feat: Add tests to Nav component

--- client/src/components/Nav.test.js
+++ client/src/components/Nav.test.js
@@ -1,24 +1,28 @@
 import React from "react";
 import {shallow} from "enzyme";
-import Nav from './Nav';
-import {UserNav} from './common/UserNav';
+import {Nav} from './Nav';
 
 
 describe("Nav", () => {
 
-  let component;
+  const nav = shallow(<Nav />);
 
-  beforeAll(() => {
-    component = shallow(<Nav />);
-    console.log(component.children().find(UserNav).length);
+  it('should render properly', function () {
+    expect(nav).toMatchSnapshot();
   });
 
   it("checks that Nav contains logo", () => {
-    console.log(component);
-    expect(component.find('#logo').length).toBe(1);
+    expect(nav.find('#logo').length).toBe(1);
   });
 
   it("checks that Nav contains UserNav component", () => {
-    expect(component.find(UserNav).length).toBe(1);
+    expect(nav.find('Connect(UserNav)')
+      .exists()).toBe(true);
+  });
+
+  it('should set opposite value', function () {
+    expect(nav.instance().state.navBar).toBe(true);
+    nav.instance().handleNavBar();
+    expect(nav.instance().state.navBar).toBe(false);
   });
 });
