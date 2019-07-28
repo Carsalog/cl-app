8271e3d85674ea8dec0eed818b2835ebee0df878
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-28 16:10:54 -0700

    refactor: Move error handling to ErrorHandler class

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -1,60 +1,13 @@
 import axios from "axios";
 import {store} from "../loader";
-import logger from "./logger";
-import {SET_MESSAGE} from "../actions/types";
+import ErrorHandler from "./errorsHandlers";
 
+const errorHandler = new ErrorHandler(store);
 
-const msg = {error: "Server isn't available"};
-
-export function setJwt (jwt) {
-  axios.defaults.headers.common["x-auth-token"] = jwt;
-}
-
-const handleError = () => {
-  /**
-   * Set default error message, and log it
-   */
-  store.dispatch({type: SET_MESSAGE, payload: msg});
-};
-
-const handleServerMessage = e => {
-  /**
-   * Set server error message, if status code 404
-   * redirect user to not found page
-   */
-  store.dispatch({type: SET_MESSAGE, payload: e.data});
-};
-
-const handleBadRequestError = e => {
-  /**
-   * Handle bad request status codes (4xx)
-   */
-  logger.log(e);
-  if (e && e.data && e.data.error) handleServerMessage(e);
-  else handleError();
-
-};
-
-
-const errorHandle = e => {
-  /**
-   * Handling errors from the server response. If server is not available,
-   * or there is not Internet connection, or remote server has a bug,
-   * raise an error.
-   *
-   * @return: null
-   */
-  console.log(e);
-  const err = e.response && e.response.status >= 400 && e.response.status < 500;
-  if (!err) handleError();
-  else handleBadRequestError(e.response);
-
-  return null;
-};
-
+const setBaseURL = url => axios.defaults.baseURL = url;
 
 axios.defaults.baseURL = store.getState().config.api;
-axios.interceptors.response.use(null, errorHandle);
+axios.interceptors.response.use(null, errorHandler.errorHandle);
 
 export default {
   get: axios.get,
@@ -63,5 +16,5 @@ export default {
   patch: axios.patch,
   delete: axios.delete,
   create: axios.create,
-  setJwt: setJwt
+  setBaseURL
 };
