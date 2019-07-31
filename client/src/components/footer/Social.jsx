9c7292bd825e627f9b047e04c79cd2508d72e786
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-31 11:56:16 -0700

    refactor: Social component

--- client/src/components/footer/Social.jsx
+++ client/src/components/footer/Social.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import {connect} from "react-redux";
 import IconLink from "../common/IconLink";
 
-export class Social extends React.Component {
+export const Social = ({social}) => social.map(item => <IconLink item={item} key={item._id} />);
 
-  render() {
-    // const {social} = this.props.config;
-    return this.props.config.social.map(item => <IconLink item={item} key={item._id} />);
-  }
-}
-
-export default connect(state => ({config: state.config}))(Social);
+export default connect(state => ({social: state.config.social}))(Social);
