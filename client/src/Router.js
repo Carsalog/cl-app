0b4b782b3d84f190eb2ad25352b3d2e437ef82fc
Oleg Dudkin <dudkin.on@gmail.com>
2019-08-19 16:02:18 -0700

    refactor: Update Router component

--- client/src/Router.js
+++ client/src/Router.js
@@ -10,20 +10,30 @@ import OAuth from "./components/OAuth";
 import NotFound from "./components/NotFound";
 import Search from "./components/Search";
 import Base from "./components/Base";
+import {store} from "./loader";
+import {setStore, messages, setMessage, user} from './services/redux';
 
-const Router = () => (
-  <Switch>
-    <Route path="/posts" component={Posts}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/logout" component={Logout}/>
-    <Route path="/register" component={Register}/>
-    <Route path="/oauth" component={OAuth}/>
-    <Route path="/search" component={Search}/>
-    <ProtectedRoute path="/profile" component={Profile}/>
-    <Route path="/not-found" component={NotFound}/>
-    <Route path="/" exact component={Base}/>
-    <Redirect from="*" to="/not-found"/>
-  </Switch>
-);
+const Router = () => {
+  setStore(store);
+
+  return (
+    <Switch>
+      <Route path="/posts" component={Posts}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/logout" component={Logout}/>
+      <Route path="/register" component={Register}/>
+      <Route path="/oauth" component={OAuth}/>
+      <Route path="/search" component={Search}/>
+      <ProtectedRoute path="/profile"
+                      component={Profile}
+                      onSetMessage={() => setMessage(messages().loginError)}
+                      user={user()}
+      />
+      <Route path="/not-found" component={NotFound}/>
+      <Route path="/" exact component={Base}/>
+      <Redirect from="*" to="/not-found"/>
+    </Switch>
+  );
+};
 
 export default Router;
