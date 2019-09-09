4448514955b1db594b9b1d68d7bf265efa560977
Oleg Dudkin <dudkin.on@gmail.com>
2019-09-09 13:18:55 -0700

    feat: Add Logo component

---
+++ client/src/components/Header/Logo.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Icon from "../common/Icon";
+import {Link} from "react-router-dom";
+
+export function mapStateToProps(state) {
+  return {
+    logo: state.config.header.logo,
+    www: state.config.header.www,
+  };
+}
+
+export class Logo extends Component {
+  render() {
+    return (
+      <Link className="nav__logo" to={this.props.www}>
+        <Icon {...this.props.logo} />
+      </Link>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+)(Logo);
