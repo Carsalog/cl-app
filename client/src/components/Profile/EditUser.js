9270a0d630334d62ef65681806ac857855a5cc28
Oleg Dudkin <dudkin.on@gmail.com>
2019-10-13 15:39:18 -0700

    refactor: EditUser component

--- client/src/components/Profile/EditUser.js
+++ client/src/components/Profile/EditUser.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {update} from "../../services/user";
-import UserInterface from "../common/UserInterface";
+import {update} from '../../services/user';
+import UserInterface from '../common/UserInterface';
 
 
 class EditUser extends UserInterface {
   constructor(props) {
-    super();
+    super(props);
 
     this.state = {
       data: {
@@ -25,24 +25,41 @@ class EditUser extends UserInterface {
     const data = {...this.state.data};
     delete data.passwordConf;
     update(data).then(res => {
-      if (res) this.props.history.push("/profile/me");
+      if (res) this.props.history.push('/profile/me');
     })
   };
 
   render() {
     return (
-      <div className="container flex-center page-hight">
-        <form onSubmit={this.handleSubmit} className="w-min-500">
-          <h1 className="h3">Edit my info</h1>
-          <hr/>
-          {this.renderInput("email", "Email", "text", false, this.handleEmail)}
-          {this.renderInput("firstName", "First name")}
-          {this.renderInput("lastName", "Last name")}
-          {this.renderInput("phone", "Phone number")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderInput("passwordConf", "Confirm password", "password")}
-          {this.renderButton("UPDATE")}
-          <hr/>
+      <div className="container profile profile--edit">
+        <form onSubmit={this.handleSubmit} className="form">
+          <div className="form__group">
+            <h1 className="profile__header">Edit my info</h1>
+          </div>
+          <div className="form__flexbox">
+            <div className="form__short">
+              {this.renderInput('email', 'Email', 'text', false, this.handleEmail)}
+            </div>
+            <div className="form__short">
+              {this.renderInput('phone', 'Phone number')}
+            </div>
+            <div className="form__short">
+              {this.renderInput('firstName', 'First name')}
+            </div>
+            <div className="form__short">
+              {this.renderInput('lastName', 'Last name')}
+            </div>
+            <div className="form__short">
+              {this.renderInput('password', 'Password', 'password')}
+            </div>
+            <div className="form__short">
+              {this.renderInput('passwordConf', 'Confirm password', 'password')}
+            </div>
+          </div>
+          <hr className="profile__hr"/>
+          <div className="form__wrapper">
+            {this.renderButton('update')}
+          </div>
         </form>
       </div>
     );
