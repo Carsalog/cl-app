a04a6b54c2438b9db2cdf98732f4729aa0a3abc9
Oleg Dudkin <dudkin.on@gmail.com>
2019-11-13 14:52:23 -0800

    refactor: Tag component

--- client/src/components/common/Tags.js
+++ client/src/components/common/Tags.js
@@ -2,25 +2,27 @@ import React from 'react';
 
 
 const Tags = ({tags, tag, onTagChange, onRemoveTag, error}) => (
-  <div className="input-group mb-5 border-bottom">
-    {tags.length > 0 && tags.map(tag =>
-      <span className="badge badge-primary tag-badge mr-2" key={tag._id}>
-        <span
-          onClick={() => onRemoveTag(tag)}
-          className="pointer link-white float-right">
-          <i className="fa fa-times" aria-hidden="true"> </i>
-        </span>
-        <span className="px-1 text-uppercase">{tag.name}</span>
-      </span>
+  <div className="form__group">
+    {tags.length > 0 && (
+      <div className="tag__container">
+        {tags.map(tag =>
+          <div className="tag" key={tag._id}>
+            <span
+              onClick={() => onRemoveTag(tag)}
+              className="tag__button">&times;</span>
+            <span className="tag__text">{tag.name}</span>
+          </div>
+      )}
+      </div>
     )}
     <input
       type="text"
       name="tag"
-      placeholder="Add tags"
+      placeholder="Add tag"
       autoFocus={true}
       value={tag}
-      className="tags-input" onChange={onTagChange}/>
-    {error && <small id={`tagHelp`} className="form-text text-danger">{error}</small>}
+      className="form__input" onChange={onTagChange}/>
+    {error && <small id={`tagHelp`} className="form__text--error">{error}</small>}
   </div>
 );
 
