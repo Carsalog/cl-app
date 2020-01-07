f99715d8f5d8ddd6ccf629beea2a01ac50522aaa
Oleg Dudkin <dudkin.on@gmail.com>
2020-01-07 11:33:49 -0800

    refactor: Update messages model

--- server/models/messages.js
+++ server/models/messages.js
@@ -1,20 +1,26 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
-const config = require("config");
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const config = require('config');
+const md5 = require('md5');
 
-const messageSchema = new mongoose.Schema({
+const message = new mongoose.Schema({
   sender: {
-    type: Schema.Types.ObjectId,
-    ref: config.get("users.tableName")
+    type: mongoose.Schema.Types.ObjectId,
+    ref: config.get('users.tableName')
+  },
+  chat: {
+    type: String,
+    trim: true,
+    required: true
   },
   receiver: {
-    type: Schema.Types.ObjectId,
-    ref: config.get("users.tableName")
+    type: mongoose.Schema.Types.ObjectId,
+    ref: config.get('users.tableName')
   },
   text: {
     type: String,
-    minlength: config.get("messages.text.min"),
-    maxlength: config.get("messages.text.max"),
+    minlength: config.get('messages.text.min'),
+    maxlength: config.get('messages.text.max'),
     required: true
   },
   date: {
@@ -32,28 +38,134 @@ const messageSchema = new mongoose.Schema({
   }
 });
 
-const Message = mongoose.model(String(config.get("messages.tableName")), messageSchema);
 
+message.statics.getByUid = function (uid) {
+  return this.aggregate(
+    [
+      {
+        '$match': {
+          '$or': [
+            {'sender': mongoose.Types.ObjectId(uid)},
+            {'receiver': mongoose.Types.ObjectId(uid)}
+          ]
+        }
+      },
+      {
+        '$group': {
+          '_id': "$chat",
+          "sender_id": {"$first": "$sender"},
+          "receiver_id": {"$first": "$receiver"},
+          'count': {'$sum': 1},
+          'messages': {'$push': '$$ROOT'}
+        }
+      },
+      {
+        "$lookup": {
+          from: 'users',
+          localField: 'sender_id',
+          foreignField: '_id',
+          as: 'user_1'
+        }
+      },
+      {
+        "$lookup": {
+          from: 'users',
+          localField: 'receiver_id',
+          foreignField: '_id',
+          as: 'user_2'
+        }
+      }
+    ]
+  );
+};
 
-async function getBySenderReceiver(senderId, receiverId) {
-  return Message.find({sender: senderId, receiver: receiverId})
-}
+message.statics.getBySenderReceiver = function (senderId, receiverId, cb) {
+  return this.aggregate(
+    [
+      {'$match': {'to': senderId}},
+      // { "$sort": { "date": 1 } },
+      // { "$group": {
+      //     "_id": "from",
+      //     "to": { "$first": "$to" },
+      //     "message": { "$first": "$message" },
+      //     "date": { "$first": "$date" },
+      //     "origId": { "$first": "$_id" }
+      //   }},
+      // { "$lookup": {
+      //     "from": "users",
+      //     "localField": "from",
+      //     "foreignField": "_id",
+      //     "as": "from"
+      //   }},
+      // { "$lookup": {
+      //     "from": "users",
+      //     "localField": "to",
+      //     "foreignField": "_id",
+      //     "as": "to"
+      //   }},
+      // { "$unwind": { "path" : "$from" } },
+      // { "$unwind": { "path" : "$to" } }
+    ],
+    cb
+  )
+  // .find({
+  //   sender: senderId,
+  //   receiver: receiverId
+  // })
+  // .sort('date')
+  // .groupBy()
+  // .limit(config.get('messages.tableName'))
+};
 
-async function create(msg) {
-  const message = Message(msg);
+message.statics.create = function (sender, receiver, text) {
+
+  const combined = [sender, receiver];
+  combined.sort();
+  const chat = md5(combined.join(''));
+
+  const message = this({sender, receiver, text, chat});
   return message.save();
-}
+};
 
-async function update(_id, text) {
+message.statics.update = async function (msg) {
 
-  const current = await Message.findById(_id);
+  const current = await this.findById(msg._id);
   if (!current) return null;
 
-  // Update and return a car type
-  current.text = text;
+  current.text = msg.text;
+  if (msg.seen !== undefined)
+    current.seen = msg.seen;
   return current.save();
-}
+};
 
-async function remove(pbjectId) {
-  return Message.findByIdAndRemove(objectId);
-}
+message.statics.remove = function (_id) {
+  return this.findByIdAndRemove(_id);
+};
+
+exports.Message = mongoose.model(String(config.get('messages.tableName')), message);
+
+module.exports.validate = function (msg) {
+  /**
+   * Validate message data from a client
+   *
+   * @type {{sender: *, receiver: *, chat: *, text: *, date: *, isActive: Boolean, seen: Boolean}}
+   * @return Object:
+   */
+  const schema = {
+    _id: Joi.string(),
+    sender: Joi.string().min(config.get("messages.id.min"))
+      .max(config.get("messages.id.max")).required(),
+    receiver: Joi.string().min(config.get("messages.id.min"))
+      .max(config.get("messages.id.max")).required(),
+    chat: Joi.string().min(config.get("messages.chat.min"))
+      .max(config.get("messages.chat.max")),
+    text: Joi.string().min(config.get("messages.text.min"))
+      .max(config.get("messages.text.max")).required(),
+    date: Joi.date(),
+    isActive: Joi.boolean(),
+    seen: Joi.boolean(),
+    __v: Joi.any()
+  };
+
+  return Joi.validate(msg, schema);
+};
